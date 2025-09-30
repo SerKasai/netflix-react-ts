02957c9f9b6172e057d5fcd768792d1abb0bc333
SerKasai <serjtheignis92@gmail.com>
2025-09-30 19:11:03 +0200

    add hover cards

--- src/components/SerieTV.tsx
+++ src/components/SerieTV.tsx
@@ -1,8 +1,9 @@
 import { useFetch } from "../hooks/FetchReducer";
+import "./SerieTV.css";
 
 type List = {
   id: number;
-  origin_language: string;
+  origin_country: string;
   original_name: string;
   overview: string;
   name: string;
@@ -36,12 +37,24 @@ function SerieTV() {
         <h2 className="p-4">Serie TV popolari</h2>
         <ul className="flex justify-between flex-wrap gap-y-5 w-full">
           {posters?.results.slice(0, 6).map((tv: List) => (
-            <li key={tv.id}>
-              <img
-                src={`${URLSearchParams.string_image}${tv.poster_path}`}
-                alt="poster"
-                className="w-72 aspect-[3/4] cursor-pointer"
-              />
+            <li key={tv.id} className="flex flex-col w-72 h-96">
+              <div id="card-container" className="h-full">
+                <img
+                  src={`${URLSearchParams.string_image}${tv.poster_path}`}
+                  alt="poster"
+                  className="w-72 aspect-[3/4] cursor-pointer img-card"
+                />
+                <div id="info" className="relative bottom-full hidden h-full">
+                  <h2 className="text-red-600 py-4">{tv.name}</h2>
+                  <h2>
+                    Titolo originale: <br />
+                    <p className="text-shadow-lg text-shadow-red-700 py-2.5">
+                      {tv.original_name} ( {tv.origin_country} )
+                    </p>
+                  </h2>
+                  <p>{tv.overview}</p>
+                </div>
+              </div>
             </li>
           ))}
         </ul>
